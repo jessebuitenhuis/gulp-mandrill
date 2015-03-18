62a43b0cf8c69e9ce053e9c685a55eb06871db18
Jesse Buitenhuis <jessebuitenhuis@gmail.com>
2015-03-18 22:54:51 +0100

    First commit

---
+++ test/test.js
@@ -0,0 +1,9 @@
+var assert = require("assert")
+
+describe('gulp-mandrill', function(){
+    describe('setup', function(){
+        it('should say hi when i run the test', function(){
+            console.log('Hi there!');
+        })
+    })
+})
